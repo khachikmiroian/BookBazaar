0adfc6c47f6b1f33ea0f5df6bf26f0cbfd9b990b
ArtakHovsepyan06 <artakhovsepyan241206@gmail.com>
2024-09-20 18:29:23 +0400

    change_artak_10

--- books/urls.py
+++ books/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
-from . import views
+from .views import BookDetailView, BookListView, AuthorListView, AuthorDetailView, HomeView, AboutUsView, success_view, \
+    canceled_view
+from subscriptions.views import create_subscription_session
 
 app_name = 'books'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('search/', views.post_search, name='search'),
-    path('about/', views.AboutUsView.as_view(), name='about_us'),
-    path('book_list/', views.BookListView.as_view(), name='book_list'),
-    path('authors/', views.AuthorListView.as_view(), name='author_list'),
-    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author_detail')
+    path('', HomeView.as_view(), name='home'),
+    path('about-us/', AboutUsView.as_view(), name='about_us'),
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('authors/', AuthorListView.as_view(), name='author_list'),
+    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author_detail'),
+    path('success/', success_view, name='success'),
+    path('canceled/', canceled_view, name='canceled'),
+    path('create-subscription-session/<int:plan_id>/', create_subscription_session, name='create_subscription_session'),
+    # Другие пути
 ]
--- books/views.py
+++ books/views.py
@@ -1,9 +1,17 @@
 from django.db.models import Q
-from django.shortcuts import render
-# from django.contrib.postgres.search import SearchVector, SearchQuery, TrigramSimilarity
-from .forms import SearchForm
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+import stripe
 from .models import Books, Author
-from django.views.generic import TemplateView, ListView, DetailView
+from subscriptions.models import BookPurchase
+from .forms import SearchForm
+
+# Убедитесь, что вы установили API-ключи Stripe
+stripe.api_key = 'sk_test_51Q00p605q7WRrvT5jTfofvlcC0vR5IBnUVApJc3Vxy8lmVsFszMN3GLoLTiux0mL00XCm9VsUn6rbScf9ys89NX400FqEnX62P'
 
 
 class HomeView(TemplateView):
@@ -13,41 +21,6 @@ class HomeView(TemplateView):
 class AboutUsView(TemplateView):
     template_name = 'books/about_us.html'
 
-
-# def post_search(request):
-#     form = SearchForm()
-#     query = None
-#     book_results = []  # Инициализация переменной book_results
-#     author_results = []  # Инициализация переменной author_results
-#
-#     if 'query' in request.GET:
-#         form = SearchForm(request.GET)
-#         if form.is_valid():
-#             query = form.cleaned_data['query']
-#
-#             # # Поиск книг
-#             # book_search_vector = SearchVector('title', weight='A')
-#             # book_search_query = SearchQuery(query)
-#             book_results = Books.objects.filter(status=Books.Status.PUBLISHED).annotate(
-#                 similarity=TrigramSimilarity('title', query),
-#             ).filter(similarity__gt=0.1).order_by('-similarity')
-#
-#             # # Поиск авторов
-#             # author_search_vector = SearchVector('name', weight='A')
-#             # author_search_query = SearchQuery(query)
-#             author_results = Author.objects.annotate(
-#                 similarity=TrigramSimilarity('first_name', query),
-#             ).filter(similarity__gt=0.1).order_by('-similarity')
-#
-#             author_results = author_results.distinct()
-#             book_results = book_results.distinct()
-#
-#     return render(request,
-#                   'search.html',
-#                   {'form': form,
-#                    'query': query,
-#                    'author_results': author_results,
-#                    'book_results': book_results})
 
 def post_search(request):
     form = SearchForm()
@@ -82,7 +55,12 @@ def post_search(request):
 class BookListView(ListView):
     model = Books
     template_name = 'books/book_list.html'
+    context_object_name = 'books'
     paginate_by = 6
+
+    def get_queryset(self):
+        # Фильтруем только опубликованные книги
+        return Books.objects.filter(status='PB')
 
 
 class AuthorListView(ListView):
@@ -91,9 +69,41 @@ class AuthorListView(ListView):
     paginate_by = 6
 
 
+stripe.api_key = 'YOUR_STRIPE_SECRET_KEY'
+
+
+@method_decorator(login_required, name='dispatch')
 class BookDetailView(DetailView):
     model = Books
     template_name = 'books/book_detail.html'
+
+    def post(self, request, *args, **kwargs):
+        book = self.get_object()
+
+        # Создание сессии Stripe
+        session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': book.title,
+                        },
+                        'unit_amount': int(book.price * 100),  # Цена в центах
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=request.build_absolute_uri(reverse_lazy('books:success')),
+            cancel_url=request.build_absolute_uri(reverse_lazy('books:canceled')),
+        )
+
+        # Сохранение покупки в модели BookPurchase
+        BookPurchase.objects.create(user=request.user, book=book)
+
+        return redirect(session.url, code=303)
 
 
 class AuthorDetailView(DetailView):
@@ -103,3 +113,12 @@ class AuthorDetailView(DetailView):
 
 class Contact(TemplateView):
     template_name = 'books/contact.html'
+
+
+# Представления для успешного и отмененного платежей
+def success_view(request):
+    return render(request, 'books/success.html')
+
+
+def canceled_view(request):
+    return render(request, 'books/canceled.html')
