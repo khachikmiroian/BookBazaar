4f28272a65a91dd32523df9e7bee68f75f328b1e
Xch <miroyanxcho777@gmail.com>
2024-09-23 19:26:56 +0400

    changessss

--- accounts/models.py
+++ accounts/models.py
@@ -3,16 +3,14 @@ from django.conf import settings
 from books.models import Books
 from django.utils import timezone
 from datetime import timedelta
-
+from subscriptions.models import Subscription
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=16)
     last_name = models.CharField(max_length=16)
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d/',
-                              blank=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
     purchased_books = models.ManyToManyField(Books, related_name='purchasers', blank=True)
     bookmarks = models.ManyToManyField(Books, related_name='bookmarked_by', blank=True)
     trial_end_date = models.DateTimeField(default=timezone.now() + timedelta(days=3))
